387e00bb3a267023034287ec8ba76ba7cefaafa3
munasakuta <muna.sakasakuta@gmail.com>
2023-01-13 11:42:32 +0900

    お問い合わせ機能 css以外は完成

--- src/components/inquiryPage/SendMail.js
+++ src/components/inquiryPage/SendMail.js
@@ -1,11 +1,38 @@
 import { Button } from "@mui/material";
-import React from "react";
-import { doc, setDoc } from "firebase/firestore";
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import {
+  addDoc,
+  collection,
+  doc,
+  serverTimestamp,
+  setDoc,
+} from "firebase/firestore";
 import db from "../../firebase";
 import "./SendMail.css";
 import { useAuth } from "../../hooks/useAuth";
 function SendMail() {
   const { user } = useAuth();
+  const [inquiryTitle, setInquiryTitle] = useState([]);
+  const [inquiryContent, setInquiryContent] = useState([]);
+
+  async function sendInquiryForm(e) {
+    e.preventDefault();
+    try {
+      await addDoc(collection(db, "inquiry"), {
+        title: inquiryTitle,
+        text: inquiryContent,
+        from: user.uid,
+        timestamp: serverTimestamp(),
+      });
+      setInquiryTitle("");
+      setInquiryContent("");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   function testMail() {
     console.log("testMail");
     // set
@@ -19,18 +46,45 @@ function SendMail() {
     });
   }
   return (
-    <div>
-      {/* <p class="send">認証メールを送信しました。</p>
-      <p class="link">
-        メール本文に記載されたリンクから、​<br></br>登録を完了してください。​​
-      </p>
-      <p class="limit">リンクの期限は一時間です。​</p>
-      <a href="">
-        <p class="case"> メールが届かない場合</p>
-      </a> */}
-      <h2>お問い合わせページ</h2>
-      <button onClick={testMail}></button>
-      <Button onClick={testMail}>送信</Button>
+    <div className="sendMail">
+      <h2 className="sendMail-header">お問い合わせ</h2>
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "25ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <div className="sendMail-form">
+          <TextField
+            className="sendMail-title"
+            id="filled-textarea"
+            label="タイトル"
+            placeholder="ex)〇〇の機能を追加してほしい！！"
+            multiline
+            variant="filled"
+            type="text"
+            value={inquiryTitle}
+            onChange={(e) => setInquiryTitle(e.target.value)}
+          />
+          <TextField
+            className="sendMail-content"
+            id="filled-multiline-static"
+            label="お問い合わせ内容"
+            multiline
+            rows={8}
+            // defaultValue="Default Value"
+            variant="filled"
+            type="text"
+            value={inquiryContent}
+            onChange={(e) => setInquiryContent(e.target.value)}
+          />
+        </div>
+      </Box>
+      <Button type="submit" onClick={sendInquiryForm}>
+        送信
+      </Button>
     </div>
   );
 }
